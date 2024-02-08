b4634fdcf11b3be42e23dc483af0e734ff9afd71
Abhishek Hanumant Patil <abhishek.hpatil63@gmail.com>
2024-02-08 19:55:29 +0530

    Create ASP.Net MVC 8 project.

---
+++ Registration-Login-ASP-Dot-Net-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Registration_Login_ASP_Dot_Net_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
