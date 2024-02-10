b73aa46c31b913c6766c4d7425c022268ba5009d
Abhishek Hanumant Patil <abhishek.hpatil63@gmail.com>
2024-02-10 08:07:50 +0530

    SQL Query File
    Added SQL used query file inside project.

--- Registration-Login-ASP-Dot-Net-MVC/obj/Debug/net8.0/Registration-Login-ASP-Dot-Net-MVC.AssemblyInfo.cs
+++ Registration-Login-ASP-Dot-Net-MVC/obj/Debug/net8.0/Registration-Login-ASP-Dot-Net-MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Registration-Login-ASP-Dot-Net-MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8de1b591b01155fbe1a6f79abdbaace51560e98")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a69cfb580f4d9a3b6e619d6aca48c45eba701cee")]
 [assembly: System.Reflection.AssemblyProductAttribute("Registration-Login-ASP-Dot-Net-MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Registration-Login-ASP-Dot-Net-MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
